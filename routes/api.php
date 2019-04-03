2b3ee8bd796ad349af8c0b62c7a2d1473971db2c
helenodia <helenodia@gmail.com>
2019-04-03 15:58:57 +0100

    tidying file

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ $router->group(["prefix" => "articles"], function ($router) {
 
 $router->post("", "Articles@store");
 $router->get("", "Articles@index");
-$router->get("{article}", "Articles@show"); //pointg at show function in Articles controller
+$router->get("{article}", "Articles@show"); 
 $router->delete("{article}", "Articles@destroy");
-$router->put("{article}", "Articles@update"); //in curlys as you have to specify an article - can't just delete nothing
+$router->put("{article}", "Articles@update"); 
 
 $router->post("{article}/comments", "Comments@store");
 $router->get("{article}/comments", "Comments@index");
